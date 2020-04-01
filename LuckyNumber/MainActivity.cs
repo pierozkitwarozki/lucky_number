02e05dd6b430ee48d272be2696856e9ece504043
pierozkitwarozki <46139140+pierozkitwarozki@users.noreply.github.com>
2020-04-01 17:53:22 +0200

    Dodaj pliki projektów.

---
+++ LuckyNumber/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace LuckyNumber
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
+    public class MainActivity : AppCompatActivity
+    {
+        Button rollButton;
+        TextView numberTextView;
+        SeekBar rangeBar;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            SupportActionBar.Title = "My lucky number";
+            rollButton = FindViewById<Button>(Resource.Id.rollButton);
+            numberTextView = FindViewById<TextView>(Resource.Id.numberTextView);
+            rangeBar = FindViewById<SeekBar>(Resource.Id.seekBar);
+            rollButton.Click += RollButton_Click;
+            
+        }
+
+        private void RollButton_Click(object sender, System.EventArgs e)
+        {
+            Random random = new Random();
+            int result = random.Next(rangeBar.Progress) + 1;
+            numberTextView.Text = result.ToString();
+        }
+    }
+}
